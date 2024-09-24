be321107e6951be2188696494193974d77bb5e99
Jannis Domann <jannis.domann@ingenity.de>
2024-09-25 00:53:17 +0200

    [win] utility

---
+++ src/windows/util.cpp
@@ -0,0 +1,47 @@
+#include "util.h"
+
+#include <memory>
+
+void util::setFont(const PXHandle& handle) {
+    HFONT hFont = CreateFontA(
+        18,                        // Height
+        0,                         // Width
+        0,                         // Escapement
+        0,                         // Orientation
+        FW_NORMAL,                 // Weight
+        FALSE,                     // Italic
+        FALSE,                     // Underline
+        0,                         // StrikeOut
+        ANSI_CHARSET,              // CharSet
+        OUT_DEFAULT_PRECIS,        // OutPrecision
+        CLIP_DEFAULT_PRECIS,       // ClipPrecision
+        DEFAULT_QUALITY,           // Quality
+        DEFAULT_PITCH | FF_SWISS,  // PitchAndFamily
+        WIN_STANDARD_FONT          // Facename
+    );
+    SendMessage(static_cast<HWND>(handle), WM_SETFONT, (WPARAM)hFont, TRUE);
+}
+void util::setTitle(const PXHandle& handle, const std::string& title) {
+    SendMessage(static_cast<HWND>(handle), WM_SETTEXT, 0, (LPARAM)(&title));
+}
+
+std::string util::getTitle(const PXHandle& handle) {
+    int txtLen = SendMessage(static_cast<HWND>(handle), WM_GETTEXTLENGTH, 0, 0);
+    std::unique_ptr<char> buffer(new char[txtLen+1]);
+    SendMessage(static_cast<HWND>(handle), WM_GETTEXT, (WPARAM)(txtLen+1), (LPARAM)buffer.get());
+
+    if (!buffer)
+        return "";
+    return std::string(buffer.get());
+}
+
+void util::addItems(const PXHandle& handle, const std::vector<std::string>& items) {
+    SendMessage(static_cast<HWND>(handle), LB_RESETCONTENT, 0, 0);
+
+    for (size_t i=0; i<items.size(); i++)
+        SendMessage(static_cast<HWND>(handle), LB_INSERTSTRING, (WPARAM)(i), (LPARAM)(&items[i]));
+}
+
+size_t util::getSelectedItem(const PXHandle& handle) {
+    return SendMessage(static_cast<HWND>(handle), LB_GETCURSEL, 0, 0);
+}
---
+++ src/windows/util.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "include/global.h"
+#include <string>
+#include <vector>
+
+namespace util {
+    void setFont(const PXHandle& handle);
+    void setTitle(const PXHandle& handle, const std::string& title);
+    std::string getTitle(const PXHandle& handle);
+    void addItems(const PXHandle& handle, const std::vector<std::string>& items);
+    size_t getSelectedItem(const PXHandle& handle);
+}
+    
